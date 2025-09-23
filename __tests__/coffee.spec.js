40ccbafc47a7a4a4d92ef8dc99e8e91c3149d738
jack-feddersen <jack.taylor@feddersen.group>
2025-09-23 14:33:03 +0200

    (tests): make tests fail

--- __tests__/coffee.spec.js
+++ __tests__/coffee.spec.js
@@ -1,27 +1,29 @@
+// This is where you'll write tests for the coffee machine. You can't change the source code to help.
+
 const { coffeeMachine } = require("../src/coffeeMachine");
 
 describe("coffee", () => {
   test("should see how many beans are left", () => {
-    // This is where you can create a test.
+    fail();
   });
 
   test("should add more beans to the machine", () => {
-    // This is where you can create a test.
+    fail();
   });
 
   test("should use a take-away cup", () => {
-    // This is where you can create a test.
+    fail();
   });
 
   test("should make a coffee in a mug", () => {
-    // This is where you can create a test.
+    fail();
   });
 
   test("should return beans for a given mL", () => {
-    // This is where you can create a test.
+    fail();
   });
 
   test("should make a coffee for a 750mL cup", () => {
-    // This is where you can create a test.
+    fail();
   });
 });
